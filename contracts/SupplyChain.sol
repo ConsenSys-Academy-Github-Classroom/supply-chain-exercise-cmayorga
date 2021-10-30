a0dbaa6541b1f34c2644a4115602bb36d61fdac4
Carlos Mayorga <cmayorga@buclelabs.com>
2021-10-30 20:10:53 +0200

    first test

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -4,43 +4,81 @@ pragma solidity >=0.5.16 <0.9.0;
 contract SupplyChain {
 
   // <owner>
+  	address public owner;
 
   // <skuCount>
+  	uint public skuCount;
 
   // <items mapping>
+	mapping(uint => Item) items;
 
   // <enum State: ForSale, Sold, Shipped, Received>
-
+	enum State { ForSale, Sold, Shipped, Received }
   // <struct Item: name, sku, price, state, seller, and buyer>
-  
+	struct Item {
+		string name;
+		uint sku;
+		uint price;
+		State state;
+		address payable seller;
+		address payable buyer;
+	}
   /* 
    * Events
-   */
-
+   */	
   // <LogForSale event: sku arg>
-
+	event LogForSale(uint sku);
   // <LogSold event: sku arg>
-
+	event LogSold(uint sku);
   // <LogShipped event: sku arg>
-
+	event LogShipped(uint sku);
   // <LogReceived event: sku arg>
-
+	event LogReceived(uint sku);
 
   /* 
    * Modifiers
    */
-
   // Create a modifer, `isOwner` that checks if the msg.sender is the owner of the contract
-
   // <modifier: isOwner
+	modifier isOwner {
+		require(owner == msg.sender);
+		_;
+	}
+
+	modifier isSold(uint sku) {
+		require(items[sku].state == State.Sold);
+		_;
+	}
+
+	modifier isShipped(uint sku) {
+		require(items[sku].state == State.Shipped);
+		_;
+	}
+
+	modifier isForSale(uint sku) {
+		require(items[sku].state == State.ForSale);
+		_;
+	}	
+
+	modifier notSeller(uint sku) {
+		require(items[sku].seller != msg.sender);
+		_;
+	}	
+
+	modifier refund(uint sku) {
+	  if (msg.value > items[sku].price){
+		  items[sku].buyer.transfer(items[sku].price - msg.value);
+	  }
+	  _;
+	}
 
   modifier verifyCaller (address _address) { 
-    // require (msg.sender == _address); 
+     require (msg.sender == _address); 
     _;
   }
 
   modifier paidEnough(uint _price) { 
-    // require(msg.value >= _price); 
+    require(msg.value >= _price); 
     _;
   }
 
@@ -67,15 +105,26 @@ contract SupplyChain {
 
   constructor() public {
     // 1. Set the owner to the transaction sender
+	owner = msg.sender;
     // 2. Initialize the sku count to 0. Question, is this necessary?
   }
 
   function addItem(string memory _name, uint _price) public returns (bool) {
     // 1. Create a new item and put in array
+    items[skuCount] = Item({
+      name: _name, 
+      sku: skuCount, 
+      price: _price, 
+      state: State.ForSale, 
+      seller: msg.sender, 
+      buyer: address(0)
+    });
     // 2. Increment the skuCount by one
+	skuCount = skuCount + 1;
     // 3. Emit the appropriate event
+	emit LogForSale(skuCount-1);
     // 4. return true
-
+	return true;
     // hint:
     // items[skuCount] = Item({
     //  name: _name, 
@@ -102,32 +151,43 @@ contract SupplyChain {
   //    - check the value after the function is called to make 
   //      sure the buyer is refunded any excess ether sent. 
   // 6. call the event associated with this function!
-  function buyItem(uint sku) public {}
+  function buyItem(uint sku) public payable isForSale(sku) paidEnough(items[sku].price) refund(items[sku].price){
+	  items[sku].seller.transfer(items[sku].price);
+	  items[sku].buyer = msg.sender;
+	  items[sku].state = State.Sold;
+	  emit LogSold(sku);
+  }
 
   // 1. Add modifiers to check:
   //    - the item is sold already 
   //    - the person calling this function is the seller. 
   // 2. Change the state of the item to shipped. 
   // 3. call the event associated with this function!
-  function shipItem(uint sku) public {}
+  function shipItem(uint sku) public isSold(sku) verifyCaller(items[sku].seller){
+	  items[sku].state = State.Shipped;
+	  emit LogShipped(sku);
+  }
 
   // 1. Add modifiers to check 
   //    - the item is shipped already 
   //    - the person calling this function is the buyer. 
   // 2. Change the state of the item to received. 
   // 3. Call the event associated with this function!
-  function receiveItem(uint sku) public {}
+  function receiveItem(uint sku) public isShipped(sku) verifyCaller(items[sku].seller){
+	  items[sku].state = State.Received;
+	  emit LogReceived(sku);
+  }
 
   // Uncomment the following code block. it is needed to run tests
-  /* function fetchItem(uint _sku) public view */ 
-  /*   returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) */ 
-  /* { */
-  /*   name = items[_sku].name; */
-  /*   sku = items[_sku].sku; */
-  /*   price = items[_sku].price; */
-  /*   state = uint(items[_sku].state); */
-  /*   seller = items[_sku].seller; */
-  /*   buyer = items[_sku].buyer; */
-  /*   return (name, sku, price, state, seller, buyer); */
-  /* } */
+   function fetchItem(uint _sku) public view
+     returns (string memory name, uint sku, uint price, uint state, address seller, address buyer)
+   { 
+     name = items[_sku].name;
+     sku = items[_sku].sku;
+     price = items[_sku].price;
+     state = uint(items[_sku].state);
+     seller = items[_sku].seller;
+     buyer = items[_sku].buyer;
+     return (name, sku, price, state, seller, buyer);
+   } 
 }
